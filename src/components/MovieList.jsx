8ba1c6e4889945d2ca24861c5f3c5fa5bdd4b750
Beyza Keser <beyzaaakeser@gmail.com>
2024-06-03 15:47:02 +0300

    update

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import api from '../utils/api';
 
-const MovieList = ({genre}) => {
-    console.log(genre)
-  return (
-    <div>MovieList</div>
-  )
-}
+const MovieList = ({ genre }) => {
+  const [movies, setMovies] = useState([]);
 
-export default MovieList
+  useEffect(() => {
+    const params = {
+      with_genres: genre.id,
+    };
+    api
+      .get('discover/movie', { params })
+      .then((res) => setMovies(res.data.results)).catch((err)=> console.error(err));
+  }, []);
+  console.log(movies)
+  return <div>MovieList</div>;
+};
+
+export default MovieList;
