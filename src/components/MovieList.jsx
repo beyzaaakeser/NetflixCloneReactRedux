8aefecc4fb4165070b4d7e196756ecb9563fc67c
Beyza Keser <beyzaaakeser@gmail.com>
2024-06-03 15:28:14 +0300

    update

---
+++ src/components/MovieList.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const MovieList = ({genre}) => {
+    console.log(genre)
+  return (
+    <div>MovieList</div>
+  )
+}
+
+export default MovieList
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,22 +1,31 @@
-import React, { useEffect } from 'react'
-import Hero from '../components/Hero'
-import { useDispatch, useSelector } from 'react-redux'
-import { getPopular } from '../redux/actions/movieActions'
-import { getGenres } from '../redux/actions/genreActions'
+import React, { useEffect } from 'react';
+import Hero from '../components/Hero';
+import { useDispatch, useSelector } from 'react-redux';
+import { getPopular } from '../redux/actions/movieActions';
+import { getGenres } from '../redux/actions/genreActions';
+import Loader from '../components/Loader';
+import Error from '../components/Error';
+import MovieList from '../components/MovieList';
 
 const MainPage = () => {
-  const dispatch = useDispatch()
-  const {isLoading, error,genres} = useSelector(store => store.genres);
-  useEffect(()=>{
+  const dispatch = useDispatch();
+  const { isLoading, error, genres } = useSelector((store) => store.genres);
+  useEffect(() => {
     dispatch(getPopular());
     dispatch(getGenres());
-  },[])
+  }, []);
   return (
     <div>
-      <Hero/>
-
+      <Hero />
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Error />
+      ) : (
+        genres.map((genre) => <MovieList key={genre.id} genre={genre} />)
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default MainPage
+export default MainPage;
